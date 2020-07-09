b68b5cac076026384ad4ee86f2e5dbe7321b5bea
Mark Chouinard <mark@chouinard.me>
2020-07-09 14:37:33 -0700

    Formatting
    Signed-off-by: Mark Chouinard <mark@chouinard.me>

--- comment-image-gallery.php
+++ comment-image-gallery.php
@@ -29,23 +29,36 @@ require_once CIG_PATH . 'inc/class-comment-images.php';
 
 add_action( 'wp_enqueue_scripts', 'cig_scripts' );
 function cig_scripts() {
-	$ver = filemtime( CIG_PATH . 'assets/js/main.js' );
+
+	$ver     = filemtime( CIG_PATH . 'assets/js/main.js' );
 	$css_ver = filemtime( CIG_PATH . 'assets/css/cig.css' );
 	wp_enqueue_script( 'cig-fljs', CIG_URL . 'assets/js/featherlight.min.js', [], '1.7.14', true );
-	wp_enqueue_script( 'cig-flgjs', CIG_URL . 'assets/js/featherlight.gallery.min.js', [], '1.7.14', true );
-	wp_enqueue_script( 'cig-js', CIG_URL . 'assets/js/main.js', ['jquery', 'cig-fljs'], $ver, true );
-	wp_enqueue_script( 'cig-flswipe', '//cdnjs.cloudflare.com/ajax/libs/detect_swipe/2.1.1/jquery.detect_swipe.min.js', [], '2.1.1', true );
+	wp_enqueue_script( 'cig-flgjs',
+		CIG_URL . 'assets/js/featherlight.gallery.min.js',
+		[],
+		'1.7.14',
+		true );
+	wp_enqueue_script( 'cig-js',
+		CIG_URL . 'assets/js/main.js',
+		[ 'jquery', 'cig-fljs' ],
+		$ver,
+		true );
+	wp_enqueue_script( 'cig-flswipe',
+		'//cdnjs.cloudflare.com/ajax/libs/detect_swipe/2.1.1/jquery.detect_swipe.min.js',
+		[],
+		'2.1.1',
+		true );
 	wp_enqueue_style( 'cig-flcss', CIG_URL . 'assets/css/featherlight.min.css', [], '1.7.14' );
-	wp_enqueue_style( 'cig-flgcss', CIG_URL . 'assets/css/featherlight.gallery.min.css', [], '1.7.14' );
+	wp_enqueue_style( 'cig-flgcss',
+		CIG_URL . 'assets/css/featherlight.gallery.min.css',
+		[],
+		'1.7.14' );
 	wp_enqueue_style( 'dashicons' );
 	wp_enqueue_style( 'cig-style', CIG_URL . 'assets/css/cig.css', [], $css_ver );
 }
 
-//add_action( 'wpdiscuz_comment_form_before', 'cig_comment_form_gallery', 11 );
 add_action( 'genesis_after_entry_content', 'cig_comment_form_gallery', 5 );
 function cig_comment_form_gallery() {
-
-
 
 	$choco = \Chocolate\chocoloate_images();
 	// Get all comment images.
@@ -54,8 +67,8 @@ function cig_comment_form_gallery() {
 	if ( ! $images ) {
 		return;
 	}
-	// Get
-	$intro_images = $choco->first_four();
+	// Get first 5 images for immediate display.
+	$intro_images = $choco->intro_images();
 
 	$comment_ratings = WP_PLUGIN_DIR . '/wp-recipe-maker/templates/public/comment-rating.php';
 	if ( ! file_exists( $comment_ratings ) ) {
@@ -65,20 +78,20 @@ function cig_comment_form_gallery() {
 	echo '<h3 class="related-title">Reader\'s Images</h3>';
 
 	?>
-<!--	Start Intro Gallery Div-->
+	<!--	Start Intro Gallery Div-->
 	<div id="intro-gallery" class="related-posts">
 		<ul class="related-list">
-		<?php
-		$x = 1;
-		$more = count( $images ) - 5;
-		foreach( $intro_images as $id => $image ) {
-			$span = '';
-			if ( 1 === $x && 5 < count($images) ) {
-				$span = '<span>+' . $more . '</span>';
-			}
-			$x++;
-			foreach( $image['src'] as $id => $img ) {
-				$thumb = <<<THMB
+			<?php
+			$x    = 1;
+			$more = count( $images ) - 5;
+			foreach ( $intro_images as $id => $image ) {
+				$span = '';
+				if ( 1 === $x && 5 < count( $images ) ) {
+					$span = '<span>+' . $more . '</span>';
+				}
+				$x ++;
+				foreach ( $image['src'] as $id => $img ) {
+					$thumb = <<<THMB
 <li class="intro-image-container">
 <a data-link="gi-{$id}" class="intro" href="#">
 	{$img['related']}
@@ -88,30 +101,29 @@ function cig_comment_form_gallery() {
 
 THMB;
 
-				echo $thumb;
+					echo $thumb;
 
+				}
 
 			}
-
-		}
-		?>
-	</ul>
+			?>
+		</ul>
 	</div>
-<!--	End Intro Gallery Div-->
-<!--Gallery Link-->
+	<!--	End Intro Gallery Div-->
+	<!--Gallery Link-->
 	<p><a class="link" href="#" data-featherlight="#display-gallery">Gallery</a></p>
-<!--End Gallery Link-->
+	<!--End Gallery Link-->
 	<?php
 
 	echo '<div id="main-gallery-container">'; // Start Main Gallery Container
 	echo '<div id="main-gallery">';  // Start Main Gallery
 	$x = 1;
-	foreach( $images as $comment_id => $image ) {
-		foreach( $image['src'] as $id => $img ) {
+	foreach ( $images as $comment_id => $image ) {
+		foreach ( $image['src'] as $id => $img ) {
 			$related = $img['related'];
 			$display = $img['display'];
-			$rating = $image['rating'];
-			$stars = '';
+			$rating  = $image['rating'];
+			$stars   = '';
 			if ( $comment_ratings && "0" != $rating ) {
 				ob_start();
 				include( $comment_ratings );
@@ -143,14 +155,13 @@ ITEM;
 	echo '</div></div>';// End Main Gallery, Main Gallery Container
 	?>
 	<div id="display-gallery-container">
-		<!--		<div id="main-gallery" data-featherlight-gallery data-featherlight-filter="a.main">-->
 		<div id="display-gallery">
 
 			<?php
-			foreach( $images as $comment_id => $image ) {
+			foreach ( $images as $comment_id => $image ) {
 				foreach ( $image['src'] as $id => $img ) {
 					$related = $img['related'];
-					$div = <<<ITEM
+					$div     = <<<ITEM
 			<div class="gallery-item">
 				<a class="intro" data-link="gi-{$id}" id="gi-{$id}" href="#mgi-{$id}">
 					<div class="thumb-holder">{$img['related']}</div>
--- inc/class-comment-images.php
+++ inc/class-comment-images.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Chocolate;
 
 class Images {
@@ -11,13 +12,23 @@ class Images {
 	}
 
 	public function init() {
+
 		if ( ! $this->instance ) {
 			$this->instance = new Images();
 		}
+
 		return $this->instance;
 	}
 
-	public function get_images( ) {
+	/**
+	 * Return comment images
+	 *
+	 * @return array|bool
+	 * @since 1.0.0
+	 *
+	 */
+	public function get_images() {
+
 		global $post;
 
 		if ( ! $post || ! is_singular( 'post' ) || 'post' !== $post->post_type ) {
@@ -25,26 +36,29 @@ class Images {
 		}
 
 		$comments = get_comments( [ 'post_id' => $post->ID ] );
-		$images = [];
-		foreach( $comments as $comment ) {
+		$images   = [];
+		foreach ( $comments as $comment ) {
 			if ( 'Adriana' == $comment->comment_author ) {
 				continue;
 			}
 
-			$rating = get_comment_meta( $comment->comment_ID, 'wprm-comment-rating', true );
+			$rating      = get_comment_meta( $comment->comment_ID, 'wprm-comment-rating', true );
 			$attachments = get_comment_meta( $comment->comment_ID, 'wmu_attachments', true );
 			if ( $attachments ) {
 				if ( isset( $attachments['images'] ) ) {
-					$date = date( 'M j, Y', strtotime( $comment->comment_date ) );
+					$date                                      = date( 'M j, Y',
+						strtotime( $comment->comment_date ) );
 					$images[ $comment->comment_ID ]['comment'] = $comment->comment_content;
-					$images[ $comment->comment_ID ]['author'] = $comment->comment_author;
-					$images[ $comment->comment_ID ]['date'] = $date;
-					$images[ $comment->comment_ID ]['rating'] = $rating;
-					foreach( $attachments['images'] as $attach_id ) {
-//						$images[ $comment->comment_ID ]['src'][$attach_id]['orig'][] = wp_get_attachment_image_src( $attach_id, 'cig-image' );
-//						$images[ $comment->comment_ID ]['src'][$attach_id]['square'][] = wp_get_attachment_image_src( $attach_id, 'related' );
-						$images[ $comment->comment_ID ]['src'][$attach_id]['related'] = wp_get_attachment_image( $attach_id, [150,150] );
-						$images[ $comment->comment_ID ]['src'][$attach_id]['display'] = wp_get_attachment_image( $attach_id, 'cig-image' );
+					$images[ $comment->comment_ID ]['author']  = $comment->comment_author;
+					$images[ $comment->comment_ID ]['date']    = $date;
+					$images[ $comment->comment_ID ]['rating']  = $rating;
+					foreach ( $attachments['images'] as $attach_id ) {
+						//						$images[ $comment->comment_ID ]['src'][$attach_id]['orig'][] = wp_get_attachment_image_src( $attach_id, 'cig-image' );
+						//						$images[ $comment->comment_ID ]['src'][$attach_id]['square'][] = wp_get_attachment_image_src( $attach_id, 'related' );
+						$images[ $comment->comment_ID ]['src'][ $attach_id ]['related'] = wp_get_attachment_image( $attach_id,
+							[ 150, 150 ] );
+						$images[ $comment->comment_ID ]['src'][ $attach_id ]['display'] = wp_get_attachment_image( $attach_id,
+							'cig-image' );
 					}
 				}
 			}
@@ -55,7 +69,14 @@ class Images {
 		return empty( $this->images ) ? false : $this->images;
 	}
 
-	public function first_four() {
+	/**
+	 * Return first 5 images
+	 *
+	 * @return array
+	 * @since 1.0.0
+	 *
+	 */
+	public function intro_images() {
 
 		return array_slice( $this->images, 0, 5, true );
 	}
@@ -63,6 +84,8 @@ class Images {
 }
 
 function chocoloate_images() {
+
 	$images = new Images();
+
 	return $images->init();
 }
